42ba7fe7932a55430d7e679255f578e94b13b481
Greg <glindeman23@gmail.com>
2022-01-26 07:14:21 -0500

    removing node_modules and adding mongodb

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express'; 
+import mongoose from 'mongoose';
+
 const app = express(); 
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 5001; 
 
 import gifRouter from './routes/gifs.js';
 
@@ -11,3 +13,15 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 app.get('/express_backend', (req, res) => { 
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); 
 }); 
+
+mongoose.connect("mongodb://localhost:27017/details", {
+  useNewUrlParser: true
+});
+
+const connection = mongoose.connection;
+
+connection.once("open", function() {
+  console.log("Connection with MongoDB is successful");
+})
+
+import gif from './model.js';
